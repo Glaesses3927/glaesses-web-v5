d8ccf852fd96bc99489bf89e5953a3c0448eae15
Glaesses3927 <42023384+Glaesses3927@users.noreply.github.com>
2024-10-02 23:51:48 +0900

    Form作成と軽微変更

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 <script>
   import bg from '$lib/image_city.png';
   import test from '$lib/works_001.png';
+  import { PUBLIC_FORM_ACCESS_KEY } from '$env/static/public';
   import { onMount } from 'svelte';
   onMount(() => {
     const graphContainer = document.getElementById("graph-container");
@@ -36,6 +37,31 @@
     // school.merge({ branch: jobs1, commitOptions: {subject: " ",}});
     main.commit("2024/10").tag("now");
   });
+
+  let status = "waiting";
+  let disable = false;
+  // @ts-ignore
+  const handleSubmit = async data => {
+    status = 'sending';
+    disable = true;
+    const formData = new FormData(data.currentTarget)
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: json
+    });
+    const result = await response.json();
+    if (result.success) {
+        console.log(result);
+        status = "success";
+    }
+  }
 </script>
 
 <style>
@@ -54,7 +80,7 @@
     76%,100%{top: -18rem;}
   }
   #graph-container {
-    padding-bottom: 2rem !important;
+    padding-bottom: 0 !important;
   }
   .works:hover > div {
     opacity: 1;
@@ -68,7 +94,8 @@
   </div>
   <div class="relative pt-80 pl-4 min-[1400px]:pl-20 text-white">
     <h2 class="font-bold leading-normal text-4xl min-[1400px]:text-[4rem] mb-16">
-      <p class="inline-block align-top">Hi!👋 I </p>
+      <p class="sm:inline-block align-top">Hi!👋</p>
+      <p class="inline-block">I </p>
       <p class="texts inline-block align-top w-[300px] min-[1400px]:w-[500px] h-[6rem] overflow-y-hidden">
         <span>am a student.</span>
         <span>like coding.</span>
@@ -109,7 +136,7 @@
   <div id="graph-container"></div>
 </section>
 
-<section class="max-w-[900px] p-8 mx-auto flex flex-col justify-center items-center">
+<section class="max-w-[900px] p-8 mt-8 mx-auto flex flex-col justify-center items-center">
   <h2 class="mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 relative"><p class="relative z-[1]">Portfolio.</p><div class="absolute h-[10px] inset-x-0 bottom-0 bg-blue-300 opacity-60"/></h2>
   <div class="flex flex-wrap gap-5 justify-center">
     <div class="works flex flex-col shadow w-[320px] h-[180px] relative rounded-xl">
@@ -118,7 +145,7 @@
         <h3 class="font-bold relative">
           <div class="text-2xl mb-4">Portfolio</div>
         </h3>
-        <p class="">Glaessesのポートフォリオサイトです。</p>
+        <p class="text-sm">Glaessesのポートフォリオサイトです。</p>
       </div>
     </div>
     <div class="works flex flex-col shadow w-[320px] h-[180px] relative rounded-xl">
@@ -131,8 +158,54 @@
         <h3 class="font-bold relative">
           <div class="text-2xl mb-4">WebApp</div>
         </h3>
-        <p class="">現在1つのWebアプリケーションが作成されています。詳しくは右上のボタンから。</p>
+        <p class="text-sm">現在1つのWebアプリケーションが作成されています。詳しくは右上のボタンから。</p>
       </div>
     </div>
   </div>
+</section>
+
+<section class="max-w-[600px] p-8 mt-8 mx-auto flex flex-col justify-center items-center">
+  <h2 class="mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 relative"><p class="relative z-[1]">Contact.</p><div class="absolute h-[10px] inset-x-0 bottom-0 bg-gray-300 opacity-60"/></h2>
+  <form on:submit|preventDefault={handleSubmit} class="w-full">
+    <input type="hidden" name="access_key" value={PUBLIC_FORM_ACCESS_KEY}>
+    <label for="name-icon" class="block mb-2 text-sm font-medium text-gray-900">Your Name</label>
+    <div class="relative">
+      <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+        <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+        </svg>
+      </div>
+      <input type="text" id="name-icon" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5" placeholder="Your Name" disabled={disable} required>
+    </div>
+    <label for="email-address-icon" class="block mb-2 mt-6 text-sm font-medium text-gray-900">Your Email</label>
+    <div class="relative">
+      <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+        <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+          <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"/>
+          <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
+        </svg>
+      </div>
+      <input type="email" id="email-address-icon" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5" placeholder="email@glaesses.net" disabled={disable} required>
+    </div>
+    <div>
+      <label for="message" class="block mb-2 mt-6 text-sm font-medium text-gray-900">Your message</label>
+      <textarea id="message" name="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..." disabled={disable} required></textarea>
+    </div>
+    {#if status=="waiting"}
+    <button type="submit" class="mt-4 mx-auto py-2 px-16 text-sm font-bold text-center text-white rounded-lg bg-blue-600 hover:bg-blue-700 flex">
+      Send
+      <svg class="w-6 h-6 text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4"/>
+      </svg>
+    </button>
+    {:else if status=="sending"}
+    <button type="submit" class="mt-4 mx-auto py-2 px-16 text-sm font-bold text-center text-white rounded-lg bg-gray-400 flex" disabled>
+      Sending...
+    </button>
+    {:else if status=="success"}
+    <button type="submit" class="mt-4 mx-auto py-2 px-16 text-sm font-bold text-center text-white rounded-lg bg-gray-400 flex" disabled>
+      Sended!
+    </button>
+    {/if}
+  </form>
 </section>
