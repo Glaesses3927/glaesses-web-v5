7dcf67d85039a7db435e265185aca73f3dec31aa
Glaesses3927 <42023384+Glaesses3927@users.noreply.github.com>
2025-07-08 18:18:43 +0900

    test: simplify

--- svelte.config.js
+++ svelte.config.js
@@ -1,33 +1,12 @@
+import adapter from 'amplify-adapter';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-// 環境変数に基づいてアダプターを動的にインポートする
-const useStaticAdapter = process.env.DOCKER === 'true';
-
-const adapterModule = useStaticAdapter
-  ? await import('@sveltejs/adapter-static')
-  : await import('amplify-adapter');
-
-const adapter = adapterModule.default;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			useStaticAdapter
-        ? {
-          pages: 'build',
-          assets: 'build',
-          fallback: undefined,
-          precompress: false,
-          strict: true
-        } : {}
-		)
+		adapter: adapter()
 	}
 };
 
