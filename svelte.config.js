5e6478ff07a6dff12f7ad1e5b289ee6fa3a540f9
Glaesses3927 <42023384+Glaesses3927@users.noreply.github.com>
2025-07-08 16:13:32 +0900

    fix: adapter settings

--- svelte.config.js
+++ svelte.config.js
@@ -16,15 +16,18 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
+		adapter: adapter(
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+			useStaticAdapter
+        ? {
+          pages: 'build',
+          assets: 'build',
+          fallback: undefined,
+          precompress: false,
+          strict: true
+        } : {}
+		)
 	}
 };
 
