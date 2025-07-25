507bd9a1b12e45ba7e83414842f5df5839974e63
Glaesses3927 <42023384+Glaesses3927@users.noreply.github.com>
2025-07-25 22:46:57 +0900

    feat: メール送信

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,15 +1,17 @@
 import { JWT } from 'google-auth-library';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
+import jwt from 'jsonwebtoken';
 import { fail } from '@sveltejs/kit';
 import { 
   SHEET_ID, 
   GOOGLE_SERVICE_ACCOUNT_EMAIL, 
   GOOGLE_PRIVATE_KEY,
-  WEBHOOK_URL
+  WEBHOOK_URL,
+  LAMBDA_URL,
+  LAMBDA_PRIVATE_KEY,
 } from '$env/static/private';
 
 export const actions = {
-  // フォームのデフォルトアクションを定義
   default: async ({ request }) => {
     const formData = await request.formData();
     const name = formData.get('name')?.toString() || '';
@@ -17,20 +19,20 @@ export const actions = {
     const message = formData.get('message')?.toString() || '';
 
     try {
+      // スプシ更新
       const SCOPES = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive.file",
       ];
       
-      const privateKey = GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n");
-
-      const jwt = new JWT({
+      const googlePrivateKey = GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n");
+      const googleJWT = new JWT({
         email: GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        key: privateKey,
+        key: googlePrivateKey,
         scopes: SCOPES,
       });
       
-      const doc = new GoogleSpreadsheet(SHEET_ID, jwt);
+      const doc = new GoogleSpreadsheet(SHEET_ID, googleJWT);
       await doc.loadInfo();
       const inquirySheet = doc.sheetsByTitle.contact;
 
@@ -59,10 +61,32 @@ export const actions = {
 
 ${time}
 ${name} 様 (\`${email}\`) より
-> ${message}`
+>>> ${message}`
         })
       });
 
+      // メール送信
+      const lambdaPrivateKey = LAMBDA_PRIVATE_KEY.replace(/\\n/gm, "\n");
+      const payload = {};
+      const options: jwt.SignOptions = {
+        algorithm: 'RS256',
+        expiresIn: '1m',
+        issuer: 'glaessesweb-contact',
+      };
+      const token = jwt.sign(payload, lambdaPrivateKey, options);
+
+      await fetch(LAMBDA_URL, {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${token}`,
+          "address": email,
+          "name": name,
+          "content": message,
+          "inquiry_id": "000001",
+          "inquiry_date": "2025/07/25 10:30:45",
+          "need_reply": "true"
+        },
+      });
     } catch (error) {
       console.error("フォーム送信エラー:", error);
       return fail(500, { error: true, message: 'サーバーでエラーが発生しました。' });
